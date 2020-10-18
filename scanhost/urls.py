6ac031d935cb3fb40561ca3240360bdaae8b9204
lvah <976131979@qq.com>
2020-10-18 12:00:08 +0800

    relate route and views ok

---
+++ scanhost/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    # 将路由和视图函数关联起来
+    path('scanhosts/', views.scanhosts, name='scanhosts'),
+]
--- scanhost/views.py
+++ scanhost/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+# 路由-视图函数: /scanhosts/
+def scanhosts(request):
+    return HttpResponse("自动化资产扫描")
